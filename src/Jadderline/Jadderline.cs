98b057b7ca1e34ee3e2ad7400d89582524eacd39
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2024-07-05 19:20:44 -0400

    some constants

--- src/Jadderline/Jadderline.cs
+++ src/Jadderline/Jadderline.cs
@@ -1,5 +1,11 @@
 ﻿namespace Jadderline {
     public class Jadderline {
+        const float DeltaTime = 0.166667f;
+        const float FrictionNorm = 650f;
+        const float FrictionOverMax = 260f;
+        const float FrictionNormHold = FrictionNorm / 2;
+        const float FrictionOverMaxHold = FrictionOverMax / 2;
+
         public static void Run() {
             Console.WriteLine("Test");
             return;
