da67212a681870114d897bad09f42f5b0f2b17ed
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2024-07-05 19:15:32 -0400

    i think i set it up right now

---
+++ src/JadderlineTester/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Jadderline;
+
+class Program {
+    static void Main(string[] args) {
+        Jadderline.Jadderline.Run();
+    }
+}
