c52d81ceee3c586c40b5659c4a5a99f419f41480
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2024-07-06 09:38:14 -0400

    most of the code, mainly need to fill out Run and then test it

--- src/Jadderline/Jadderline.cs
+++ src/Jadderline/Jadderline.cs
@@ -7,17 +7,88 @@
         const float FrictionOverMaxHold = FrictionOverMax / 2;
 
         // Most inputs are self explanatory, though for direction, false is left and true is right
+        // Additionally, jelly1 is the one about to be grabbed, and jelly2 is the one on cooldown
         // The additional inputs may or may not need commas, not 100% sure
+        // This doesnt go back a frame if an impossible frame is reached, but ive never seen it ever go back in jadderline so its not too much of a priority currently
         public static void Run(float playerPos, float playerSpeed, float jelly1Pos, float jelly2Pos, int ladders, bool direction, bool moveOnly, string additionalInputs) {
             List<bool[]> inputs = new List<bool[]>();
             for (int i = 0; i < ladders; i++) {
+                // Get all 512 candidates
+                List<bool[]> potential = new List<bool[]>();
+                for (int j = 0; j < 512; j++) {
+                    potential.Add(ToBits(j));
+                }
 
             }
             return;
         }
 
+        // Gets the distance jelly1 has moved while ensuring the player can still grab jelly2
+        private static float Eval(bool[] inputs, float playerPos, float playerSpeed, float jelly1Pos, float jelly2Pos, bool direction) {
+            (float playerPosNew, float playerSpeedNew, float jelly1PosNew) = MoveVars(inputs, playerPos, playerSpeed, jelly1Pos, direction);
+            if (float.Abs(playerPosNew) > float.Abs(jelly2Pos)) { // Went past jelly (> is >= in the rust ver, idk why)
+                return float.PositiveInfinity;
+            } else {
+                return float.Abs(jelly1PosNew - jelly1Pos); // We want to maximize this and not player movement in order to prioritize 13px jadders when possible
+                                                            // (though the rust ver maximizes player movement so idk)
+            }
+        }
+
+        // Actually calculates the inputs
+        private static (float, float, float) MoveVars(bool[] inputs, float playerPos, float playerSpeed, float jelly1Pos, bool direction) {
+            // Frame of movement on last frame of StPickup
+            playerPos += playerSpeed * DeltaTime;
+            // 8 frames of holding the jelly
+            for (int i = 0; i < 8; i++) {
+                (playerPos, playerSpeed) = MoveStep(inputs[i], playerPos, playerSpeed, true, direction);
+            }
+            // Release jelly1, which will be at the player's current positition when dropped
+            jelly1Pos = playerPos;
+            // Frame of movement when you release the jelly
+            (playerPos, playerSpeed) = MoveStep(inputs[8], playerPos, playerSpeed, false, direction);
+            return (playerPos, playerSpeed, jelly1Pos);
+        }
+
+        // Calculates one frame of movement
+        private static (float, float) MoveStep(bool input, float playerPos, float playerSpeed, bool holding, bool direction) {
+            float frictionNorm;
+            float frictionOverMax;
+            float max;
+            if (holding) {
+                frictionNorm = FrictionNormHold;
+                frictionOverMax = FrictionOverMaxHold;
+                max = 108f;
+            } else {
+                frictionNorm = FrictionNorm;
+                frictionOverMax = FrictionOverMax;
+                max = 90f;
+            }
+            playerSpeed = float.Abs(playerSpeed);
+            if (!input) { // Holding neutral
+                playerSpeed -= frictionNorm * DeltaTime;
+                if (playerSpeed < 0f) {
+                    playerSpeed = 0f;
+                }
+            } else if (playerSpeed <= max) { // Coming up to max speed
+                playerSpeed += frictionNorm * DeltaTime;
+                if (playerSpeed > max) {
+                    playerSpeed = max;
+                }
+            } else { // Over max speed
+                playerSpeed -= frictionOverMax * DeltaTime;
+                if (playerSpeed < max) {
+                    playerSpeed = max;
+                }
+            }
+            if (!direction) { // Flip speed back if moving left
+                playerSpeed *= -1;
+            }
+            playerPos += playerSpeed * DeltaTime;
+            return (playerPos, playerSpeed);
+        }
+
         // Converts an int into a bool[9]
-        public static bool[] ToBits(int num) {
+        private static bool[] ToBits(int num) {
             bool[] bits = new bool[9];
             for (int i = 0; i < 9; i++) {
                 bits[i] = (num & 1) != 0;
