d1a27d6a00dce074b84b3266fc0b7a735db7b685
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2024-07-06 09:08:18 -0400

    add ToBits

--- src/Jadderline/Jadderline.cs
+++ src/Jadderline/Jadderline.cs
@@ -9,10 +9,21 @@
         // Most inputs are self explanatory, though for direction, false is left and true is right
         // The additional inputs may or may not need commas, not 100% sure
         public static void Run(float playerPos, float playerSpeed, float jelly1Pos, float jelly2Pos, int ladders, bool direction, bool moveOnly, string additionalInputs) {
+            List<bool[]> inputs = new List<bool[]>();
             for (int i = 0; i < ladders; i++) {
 
             }
             return;
+        }
+
+        // Converts an int into a bool[9]
+        public static bool[] ToBits(int num) {
+            bool[] bits = new bool[9];
+            for (int i = 0; i < 9; i++) {
+                bits[i] = (num & 1) != 0;
+                num >>= 1;
+            }
+            return bits;
         }
 
         // Formats the inputs to be copy and pasted into Studio
